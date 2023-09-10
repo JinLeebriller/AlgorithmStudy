a5275ba2ca28475a7506c16ba445467364e113cc
JinLeebriller <137128382+JinLeebriller@users.noreply.github.com>
2023-09-10 09:59:00 +0900

    Add files via upload

---
+++ AlgorithmStudy/rlatjsrnr/algorithmStudy/직각이등변삼각형-출력.java
@@ -0,0 +1,39 @@
+// Q. 직각이등변삼각형을 출력하는 부분을 아래와 같은 형식의 메서드로 작성하세요.
+public class Solution {
+	
+	// 왼쪽 아래가 직각인 이등변삼각형을 출력
+	/*
+		 *
+		 **
+		 ***
+		 ****
+	*/
+	static void triangleLB(int n) 
+	
+	// 왼쪽 위가 직각인 이등변삼각형을 출력
+	/*
+		 ****
+		 ***
+		 **
+		 *
+	*/
+	static void triangleLU(int n) 
+	
+	// 오른쪽 위가 직각인 이등변삼각형을 출력
+	/*
+		 ****
+		  ***
+		   **
+		    *
+	*/
+	static void triangleRU(int n) 
+	
+	// 오른쪽 아래가 직각인 이등변삼각형을 출력
+	/*
+		    *
+		   **
+		  ***
+		 ****
+	*/
+	static void triangleRB(int n) 
+}
