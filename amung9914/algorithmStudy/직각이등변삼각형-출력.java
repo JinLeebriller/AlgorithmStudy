16434e50e30d2560c5db152352a5c81387a01ccb
amung9914 <137124338+amung9914@users.noreply.github.com>
2023-09-11 16:37:15 +0900

    Update 직각이등변삼각형-출력.java

--- amung9914/algorithmStudy/직각이등변삼각형-출력.java
+++ amung9914/algorithmStudy/직각이등변삼각형-출력.java
@@ -1,6 +1,10 @@
 // Q. 직각이등변삼각형을 출력하는 부분을 아래와 같은 형식의 메서드로 작성하세요.
 public class Solution {
 	
+	public static void main(String[] args) {
+		triangleRB(4);
+	}
+	
 	// 왼쪽 아래가 직각인 이등변삼각형을 출력
 	/*
 		 *
@@ -8,7 +12,14 @@ public class Solution {
 		 ***
 		 ****
 	*/
-	static void triangleLB(int n) 
+	static void triangleLB(int n){
+		for(int i = 0; i<n; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");		
+			}
+				System.out.println();
+		}
+	} 
 	
 	// 왼쪽 위가 직각인 이등변삼각형을 출력
 	/*
@@ -17,7 +28,14 @@ public class Solution {
 		 **
 		 *
 	*/
-	static void triangleLU(int n) 
+	static void triangleLU(int n){
+		for(int i = n; i>0; i--) {
+			for(int j=i; j>0; j--) {
+				System.out.print("*");	
+			}
+			System.out.println();	
+		}
+	}
 	
 	// 오른쪽 위가 직각인 이등변삼각형을 출력
 	/*
@@ -26,7 +44,18 @@ public class Solution {
 		   **
 		    *
 	*/
-	static void triangleRU(int n) 
+	static void triangleRU(int n){
+		for(int i = 0; i<n; i++) {
+			for(int j=0; j<=n; j++) {
+				if(j>i) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+				System.out.println();
+		}
+	} 
 	
 	// 오른쪽 아래가 직각인 이등변삼각형을 출력
 	/*
@@ -35,5 +64,17 @@ public class Solution {
 		  ***
 		 ****
 	*/
-	static void triangleRB(int n) 
+	static void triangleRB(int n){
+		for(int i = n; i>0; i--) {
+			for(int j=0; j<=n; j++) {
+				if(i<j+1) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();	
+		}
+	} 
 }
+
