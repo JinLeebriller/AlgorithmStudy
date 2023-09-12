3ff5e3ba52bf80090551ac191c7e7101dc18ecea
JinLeebriller <137128382+JinLeebriller@users.noreply.github.com>
2023-09-12 09:26:13 +0900

    add: 기수변환문제 풀이

---
+++ JinLeebriller/algorithmStudy/기수변환문제.java
@@ -0,0 +1,53 @@
+package algorithmStudy;
+
+import java.util.Scanner;
+
+// Q6. 기수 변환 과정을 자세히 나타내는 프로그램을 작성하세요.
+public class 기수변환문제 {
+	public static void main(String[] args) {
+		
+		Scanner stdIn = new Scanner(System.in);
+		int decimal = 0;
+		int num = 0;
+		int index = 0;
+		char[] cno = new char[32];
+		String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		
+		System.out.println("10진수를 기수 변환합니다.");
+		System.out.print("변환하는 음이 아닌 정수:");
+		do {
+			decimal = stdIn.nextInt();
+		}while(decimal < 0);
+		
+		System.out.print("어떤 진수로 변환할까요?(2~36):");
+		do {
+			num = stdIn.nextInt();
+		}while(num < 2 || num > 36);
+		
+		System.out.printf("%4d|%8d\n",num,decimal);
+		System.out.println("    +---------");
+		
+		do {
+			cno[index++] = dchar.charAt(decimal % num);
+			decimal /= num;
+			if(decimal == 0) {
+				System.out.println("            " + decimal + " ... " + cno[index-1]);
+				break;
+			}
+			System.out.printf("%4d|%8d ...%2s\n",num,decimal,cno[index-1]);
+			System.out.println("    +---------");
+		}while(true);
+		
+		for(int i = 0 ; i < index / 2 ; i++) {
+			char temp = cno[i];
+			cno[i] = cno[index - i - 1];
+			cno[index - i - 1] = temp;
+		}
+		
+		System.out.print(num + "진수로 ");
+		for(int i = 0 ; i < index ; i++) {
+			System.out.print(cno[i]);
+		}
+		System.out.println("입니다.");
+	}
+}
