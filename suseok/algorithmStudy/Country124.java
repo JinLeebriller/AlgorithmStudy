0ba5c0b86681ac7196084e59de1375e1e98fe813
suseok <137128402+smetmoney@users.noreply.github.com>
2023-09-16 11:22:29 +0900

    Create Country124.java

---
+++ suseok/algorithmStudy/Country124.java
@@ -0,0 +1,34 @@
+package suseok;
+
+import java.util.Scanner;
+
+public class Country124 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("숫자를 입력하세요 : ");
+        int n = scanner.nextInt();
+
+        String result = solution(n);
+        System.out.println(n + "을 124 나라에서 사용하는 숫자로 변환하면 " + result + "입니다.");
+
+        scanner.close();
+    }
+    
+    public static String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+
+        while (n > 0) {
+            int remainder = n % 3;
+            if (remainder == 0) {
+                remainder = 4;
+                n = (n / 3) - 1;
+            } else {
+                n = n / 3;
+            }
+            answer.insert(0, remainder);
+        }
+
+        return answer.toString();
+    }
+}
