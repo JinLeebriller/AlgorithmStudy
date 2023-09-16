95b9426353bb998f75481c57ce0a90c65aa5bebd
suseok <137128402+smetmoney@users.noreply.github.com>
2023-09-16 12:01:32 +0900

    Update Decimal.java

--- suseok/algorithmStudy/Decimal.java
+++ suseok/algorithmStudy/Decimal.java
@@ -16,13 +16,13 @@ public class Decimal {
 
             int base;
             while (true) {
-                System.out.print("변환할 진수 (2, 8, 16, 32 중 하나 입력): ");
+                System.out.print("변환할 진수 (2, 4, 8, 16, 32 중 하나 입력): ");
                 base = scanner.nextInt();
 
-                if (base == 2 || base == 8 || base == 16 || base == 32) {
+                if (base == 2 || base == 4 ||base == 8 || base == 16 || base == 32) {
                     break;
                 } else {
-                    System.out.println("2, 8, 16, 32 중 하나를 입력하세요.");
+                    System.out.println("2, 4, 8, 16, 32 중 하나를 입력하세요.");
                 }
             }
 
