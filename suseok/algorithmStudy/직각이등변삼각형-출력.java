19e1c419359254ca4ee31014f3d916803eedfe7e
suseok <137128402+smetmoney@users.noreply.github.com>
2023-09-16 11:31:21 +0900

    Update 직각이등변삼각형-출력.java

--- suseok/algorithmStudy/직각이등변삼각형-출력.java
+++ suseok/algorithmStudy/직각이등변삼각형-출력.java
@@ -1,39 +1,68 @@
-// Q. 직각이등변삼각형을 출력하는 부분을 아래와 같은 형식의 메서드로 작성하세요.
+import java.util.Scanner;
+
 public class Solution {
-	
-	// 왼쪽 아래가 직각인 이등변삼각형을 출력
-	/*
-		 *
-		 **
-		 ***
-		 ****
-	*/
-	static void triangleLB(int n) 
-	
-	// 왼쪽 위가 직각인 이등변삼각형을 출력
-	/*
-		 ****
-		 ***
-		 **
-		 *
-	*/
-	static void triangleLU(int n) 
-	
-	// 오른쪽 위가 직각인 이등변삼각형을 출력
-	/*
-		 ****
-		  ***
-		   **
-		    *
-	*/
-	static void triangleRU(int n) 
-	
-	// 오른쪽 아래가 직각인 이등변삼각형을 출력
-	/*
-		    *
-		   **
-		  ***
-		 ****
-	*/
-	static void triangleRB(int n) 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("높이를 입력하세요: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        triangleLB(n);
+        System.out.println();
+
+        triangleLU(n);
+        System.out.println();
+
+        triangleRU(n);
+        System.out.println();
+
+        triangleRB(n);
+        System.out.println();
+    }
+
+    // 왼쪽 아래가 직각인 이등변삼각형을 출력
+    static void triangleLB(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // 왼쪽 위가 직각인 이등변삼각형을 출력
+    static void triangleLU(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // 오른쪽 위가 직각인 이등변삼각형을 출력
+    static void triangleRU(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = n; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // 오른쪽 아래가 직각인 이등변삼각형을 출력
+    static void triangleRB(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
