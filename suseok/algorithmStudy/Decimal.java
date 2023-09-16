405920d8303de19332d09b7048c48d31fb68b3a7
suseok <137128402+smetmoney@users.noreply.github.com>
2023-09-16 11:38:23 +0900

    Update Decimal.java

--- suseok/algorithmStudy/Decimal.java
+++ suseok/algorithmStudy/Decimal.java
@@ -6,23 +6,31 @@ public class Decimal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("입력하실 정수: ");
-        int enterInt = scanner.nextInt();
-
-        int base;
         while (true) {
-            System.out.print("변환할 진수 (2, 8, 16, 32 중 하나 입력): ");
-            base = scanner.nextInt();
+            System.out.print("입력하실 정수 (0 입력하면 종료): ");
+            int enterInt = scanner.nextInt();
 
-            if (base == 2 || base == 8 || base == 16 || base == 32) {
+            if (enterInt == 0) {
                 break;
-            } else {
-                System.out.println("2, 8, 16, 32 중 하나를 입력하세요.");
             }
+
+            int base;
+            while (true) {
+                System.out.print("변환할 진수 (2, 8, 16, 32 중 하나 입력): ");
+                base = scanner.nextInt();
+
+                if (base == 2 || base == 8 || base == 16 || base == 32) {
+                    break;
+                } else {
+                    System.out.println("2, 8, 16, 32 중 하나를 입력하세요.");
+                }
+            }
+
+            String result = convertDecimal(enterInt, base);
+            System.out.println(base + "진수로 " + result + "입니다.");
         }
-        
-        String result = convertDecimal(enterInt, base);
-        System.out.println(base + "진수로 " + result + "입니다.");
+
+        scanner.close();
     }
 
     public static String convertDecimal(int enterInt, int base) {
@@ -37,4 +45,3 @@ public class Decimal {
         return result.toString();
     }
 }
-
