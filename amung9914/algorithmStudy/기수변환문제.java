aa98a48ae7e3ef35235db53b83d78685338d05a0
amung9914 <137124338+amung9914@users.noreply.github.com>
2023-10-01 00:45:58 +0900

    refactor: 직각이등변삼각형 출력 문제 리팩토링

--- amung9914/algorithmStudy/기수변환문제.java
+++ amung9914/algorithmStudy/기수변환문제.java
@@ -1,70 +1,37 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
-/*
-문제1.
-
-아래 예시처럼 기수 변환 과정을
-자세히 나타내는 프로그램을
-작성하세요. (아래 예시의 59, 2라는 값은 Scanner로 입력 받은 값입니다.)
-
-Console창)
-
-10진수를 기수 변환합니다.
-변환하는 음이 아닌 정수 : 59
-어떤 진수로 변환할까요? (2~36): 2
-
-2 |        59
-  +————-
-2 |        29    •••1
-  +————-
-(•••생략•••)
-2 |           1    •••1
-  +————-
-               0    •••1
-2진수로 111011입니다.
-
-*/
-public class Main2 {
+import java.util.*;
+/* 손코딩 버전 */
+public class Jinsu {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char[] arr = new char[35];
+        String sample = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int count = 0;
 
-        solution();
-    }
+        System.out.println("10진수를 기수변환합니다");
+        System.out.print("변환하는 음이 아닌 정수:");
+        int target = scan.nextInt();
 
-    // 59 : x , 2 : y
+        System.out.print("어떤 진수로 변환할까요?(2~36)");
+        int n = scan.nextInt();
 
-    public static void solution () {
-
-        String str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        List<String> dchar = Arrays.asList(str.split(""));
-        System.out.println(dchar.get(14));
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("10진수 입력 : ");
-        int target = scanner.nextInt();
-        System.out.println("변환할 진수 입력 : ");
-        int jinsu = scanner.nextInt();
-
-        int nameoji = target%jinsu;
-        StringBuilder sb = new StringBuilder();
-        System.out.printf("%d|   %d%n",jinsu,target);
-        System.out.println(" +---------");
-
-        while(true){
-            // System.out.printf("%d|   %d ... %d %n",jinsu,target/jinsu,target%jinsu);
-            System.out.printf("%d|   %d ... %s %n",jinsu,target/jinsu,dchar.get(target%jinsu));
-            System.out.println(" +---------");
-            sb.append(dchar.get(target%jinsu));
-            target = target/jinsu;
-            if(target/jinsu<=1){
-                System.out.printf("       %d ... %s%n",target/jinsu,dchar.get(target%jinsu));
-                sb.append(dchar.get(target%jinsu));
-                break;
+        System.out.println(n+"|         "+target);
+        System.out.println(" +-------------------");
+        do{
+            // String타입.charAt(인덱스번호) == 문자열에서 원하는 위치의 문자를 char타입으로 변환한다
+            arr[count]=sample.charAt(target%n);
+            target = target/n;
+            if(target==0){
+                System.out.println("         "+target+"..."+arr[count]);
+            }else{
+                System.out.println(n+"|         "+target+"..."+arr[count]);
+                System.out.println(" +-------------------");
+                count++;
             }
+        }while(target>0);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<=count;i++){
+            sb.append(arr[i]);
         }
-        System.out.println(jinsu+"진수로 "+ sb.reverse()+"입니다.");
-
+        System.out.println(n+"진수로"+sb.reverse()+"입니다.");
     }
 }
