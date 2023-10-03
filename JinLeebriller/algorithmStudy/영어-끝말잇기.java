3455c98002c484805b5817afe3ead7bdec94bf4a
Jinhyeong Lee <137128382+JinLeebriller@users.noreply.github.com>
2023-10-03 17:00:21 +0900

    add: 영어 끝말잇기 풀이

---
+++ JinLeebriller/algorithmStudy/영어-끝말잇기.java
@@ -0,0 +1,21 @@
+package algorithmStudy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        List<String> list = new ArrayList<>();
+        
+        for(int i = 0 ; i < words.length ; i++) {
+        	if(i > 0 && words[i].charAt(0) != words[i-1].charAt(words[i-1].length()-1) || list.contains(words[i])) {
+        		answer[0] = (i%n)+1; 
+        		answer[1] = (i/n)+1; 
+        		break;
+        	}
+        	list.add(words[i]);
+        }
+        return answer;
+    }
+}
