2cac5cb65321204f208dd005f064e3e696031e3f
rlatjsrnr <137128415+rlatjsrnr@users.noreply.github.com>
2023-10-12 17:06:56 +0900

    add: 피보나치_수 풀이

---
+++ rlatjsrnr/algorithmStudy/피보나치_수.java
@@ -0,0 +1,15 @@
+class Solution {        
+    public int solution(int n) {        
+        if(n <= 1) return n;        
+        
+        int answer = 0;
+        int n1 = 0;
+        int n2 = 1;        
+        for(int i=2; i<=n; i++){
+            answer = (n1 + n2) % 1234567;
+            n1 = n2;
+            n2 = answer;
+        }        
+        return answer;
+    }
+}
