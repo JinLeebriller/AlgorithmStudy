648541d325d52ab70f121e147624fb3f1cd3e46b
rlatjsrnr <137128415+rlatjsrnr@users.noreply.github.com>
2023-10-19 14:58:13 +0900

    add: 시간_계산기 풀이

---
+++ rlatjsrnr/algorithmStudy/시간_계산기.java
@@ -0,0 +1,35 @@
+public class Time {
+    public static void main(String[] args) {
+        String curr = "23:50:46";
+        String time = "04:14:24";
+        String result = sol(curr, time);
+        System.out.println(result);
+    }
+
+    public static String sol(String curr, String time){
+        String[] times = time.split(":");
+        int sec = Integer.parseInt(times[0]) * 3600 + Integer.parseInt(times[1]) * 60 + Integer.parseInt(times[2]);
+
+        String[] currTime = curr.split(":");
+        int answerSec = Integer.parseInt(currTime[2]) + sec;
+        int answerMin = Integer.parseInt(currTime[1]);
+        int answerHour = Integer.parseInt(currTime[0]);
+
+        if(answerSec >= 60){
+            int temp = answerSec / 60;
+            answerSec %= 60;
+            answerMin += temp;
+
+            if(answerMin >= 60){
+                temp = answerMin / 60;
+                answerMin %= 60;
+                answerHour += temp;
+
+                if(answerHour >= 24){
+                    answerHour -= 24;
+                }
+            }
+        }
+        return answerHour + ":" + answerMin + ":" + answerSec;
+    }
+}
