2ba1908481e43b45d6e4df8ed2a78a0232ad5fb5
Jinhyeong Lee <137128382+JinLeebriller@users.noreply.github.com>
2023-10-24 13:45:58 +0900

    Update and rename JinLeebriller/algorithmStudy/직각이등변삼각형-출력.java to JinLeebriller/algorithmStudy/직각이등변삼각형 출력/직각이등변삼각형-출력.java

--- JinLeebriller/algorithmStudy/직각이등변삼각형 출력/직각이등변삼각형-출력.java
+++ JinLeebriller/algorithmStudy/직각이등변삼각형 출력/직각이등변삼각형-출력.java
@@ -1,4 +1,3 @@
-// Q. 직각이등변삼각형을 출력하는 부분을 아래와 같은 형식의 메서드로 작성하세요.
 public class Solution {
 	// 왼쪽 아래가 직각인 이등변삼각형을 출력
 	static void triangleLB(int n) {
@@ -46,9 +45,5 @@ public class Solution {
 			}
 			System.out.println();
 		}
-	}
-	
-	public static void main(String[] args) {
-		triangleRB(5);
 	}
 }
