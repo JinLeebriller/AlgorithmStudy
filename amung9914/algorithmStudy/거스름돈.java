64f646c5e7d746dfed623d1b7dfc32869ad6a3da
amung9914 <137124338+amung9914@users.noreply.github.com>
2023-10-27 20:10:14 +0900

    refactor: 거스름돈 문제 리팩토링

--- amung9914/algorithmStudy/거스름돈.java
+++ amung9914/algorithmStudy/거스름돈.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 public class Test1027 {
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("물건의 가격을 입력하세요");
-        int price = scanner.nextInt();
+        int price = 1000-scanner.nextInt();
         System.out.println(balance(price));
     }
     public static int balance(int price){
-        int original = 1000-price;
         int count = 0;
         int[] arr = {500,100,50,10,5,1};
-        count += original/arr[0];
+        count += price/arr[0];
         for(int i = 0; i<arr.length-1; i++){
-            count += (original%arr[i])/arr[i+1];
+            count += (price%arr[i])/arr[i+1];
         }
         return count;
     }
