49b9a44b69893346a8b4624e294b11414faf9c6d
rlatjsrnr <137128415+rlatjsrnr@users.noreply.github.com>
2023-11-05 15:47:58 +0900

    add 게임게발 풀이

--- rlatjsrnr/algorithmStudy/게임게발.java
+++ rlatjsrnr/algorithmStudy/게임게발.java
@@ -21,18 +21,20 @@ public class Algorithm_10_5_2 {
 
     public static int sol(int x, int y, int d, String[][] map){
         int result = 1;
+        // 이동할 방향 체크
         int[] dx={0,1,0,-1};
         int[] dy={-1,0,1,0};
-
+        // 뒤로 이동할 방향 체크
         int[] backX = {1,0,-1,0};
         int[] backY = {0,-1,0,1};
-
+        // 회전수
         int count = 0;
 
         while(true){
             int destX = x + dx[d%4];
             int destY = y + dy[d%4];
             count++;
+            // 이동 가능
             if(map[destX][destY].equals("0")){
                 map[x][y] = "1";
                 d = (d%4);
@@ -41,13 +43,16 @@ public class Algorithm_10_5_2 {
                 result++;
                 count = 0;
             }else{
+                // 이동 불가능 회전 덜 함
                 if(count < 4){
                     d = (d%4);
                 }else{
+                    // 이동 불가능 회전 다 함, 뒤로 이동 가능
                     if(map[x+backX[d]][y+backY[d]].equals("0")){
                         x = x+backX[d];
                         y = y+backY[d];
                     }else{
+                        // 이동 불가능 회전 다 함, 뒤로 이동 불가능
                         break;
                     }
                 }
