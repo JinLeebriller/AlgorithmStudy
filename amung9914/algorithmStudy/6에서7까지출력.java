3ebaaf74679d5bba4c41b306264a25255099a6ca
amung9914 <137124338+amung9914@users.noreply.github.com>
2023-11-23 23:36:10 +0900

    refactor: 6에서7까지출력 문제 리팩토링

--- amung9914/algorithmStudy/6에서7까지출력.java
+++ amung9914/algorithmStudy/6에서7까지출력.java
@@ -2,6 +2,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SixToSeven {
+    // 함수안쓰고 푸는 버전
+    public static void main(String[] args){
+        String answer = "";
+        String s = "98451984614487541689572165421";
+        String temp = "";
+        char[] arr = s.toCharArray();
+        boolean b = false;
+        for(int i = 0; i< arr.length; i++){
+            if(arr[i]=='6'){
+                b = true;
+            }
+            if(b){
+                temp += arr[i];
+            }
+            if(arr[i]=='7'||i==arr.length-1){
+                if(answer.equals("")){
+                    answer += temp;
+                }else{
+                    answer += "    " + temp;
+                }
+                temp = "";
+                b = false;
+            }
+        }
+        System.out.println(answer);
+    }
+    
+    /* 함수 쓰고 푼 버
     public static void main(String[] args){
         List<String> answerList = new ArrayList<>();
         String s = "98451984614487541689572165421";
@@ -25,4 +53,5 @@ public class SixToSeven {
             System.out.print(str+"  ");
         }
     }
+    */
 }
