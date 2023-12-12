3ebe23a70e7a5b9373af986e203f90723d5918d8
Jinhyeong Lee <137128382+JinLeebriller@users.noreply.github.com>
2023-12-12 22:23:02 +0900

    add: 인사성 밝은 곰곰이 풀이

---
+++ JinLeebriller/algorithmStudy/인사성 밝은 곰곰이/인사성-밝은-곰곰이.java
@@ -0,0 +1,34 @@
+package algorithmStudy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<String> inputList = new ArrayList<>();
+        int count = 0;
+
+        for(int i = 0 ; i < N ; i++) {
+            String input = br.readLine();
+
+            if(i == 0 && !input.equals("ENTER")) {
+                System.out.println("첫 번째 주어지는 문자열은 무조건 ENTER이어야 합니다.");
+                i--;
+                continue;
+            }
+
+            if(!input.equals("ENTER") && !inputList.contains(input)) count++;
+            if(i != 0 && input.equals("ENTER")) for(int j = inputList.size() - 1 ; j >= 0 ; j--) inputList.remove(j);
+
+            inputList.add(input);
+        }
+
+        System.out.println(count);
+        br.close();
+    }
+}
