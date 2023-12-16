103d4e4da02c75ffcec75d6a4d2130998f0611b3
rlatjsrnr <137128415+rlatjsrnr@users.noreply.github.com>
2023-12-16 15:44:26 +0900

    add: ??! 풀이

---
+++ rlatjsrnr/algorithmStudy/??!.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class QQE {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String answer = scan.next() + "??!";
+        System.out.println(answer);
+    }
+}
