926b781724c93380a4c755800b27a578a40244bd
Jinhyeong Lee <137128382+JinLeebriller@users.noreply.github.com>
2024-01-01 16:08:09 +0900

    add: 영어가 싫어요 풀이

---
+++ JinLeebriller/algorithmStudy/영어가 싫어요/영어가-싫어요.java
@@ -0,0 +1,16 @@
+package algorithmStudy;
+
+public class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        String[] number = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for(int i = 0 ; i < number.length ; i++) {
+            numbers = numbers.replaceAll(number[i], String.valueOf(i));
+        }
+
+        answer = Long.parseLong(numbers);
+
+        return answer;
+    }
+}
