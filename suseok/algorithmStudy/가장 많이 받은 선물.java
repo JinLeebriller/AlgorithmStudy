19ec62dd6272be7854f5f171d8ca3eea98649bf9
suseok <137128402+smetmoney@users.noreply.github.com>
2024-01-21 05:46:00 +0900

    add : 가장 많이 받은 선물 풀이
    add : 가장 많이 받은 선물 풀이

--- suseok/algorithmStudy/가장 많이 받은 선물.java
+++ suseok/algorithmStudy/가장 많이 받은 선물.java
@@ -1,1 +1,66 @@
 
+package suseok;
+
+import java.util.HashMap;
+
+public class Many_Gift {
+
+    public static int solution(String[] friends, String[] gifts) {
+    	
+        int answer = 0;
+        
+        int friendsLength = friends.length;
+        
+        HashMap<String, Integer> dic = new HashMap<>();
+        
+        int[] giftCheck = new int[friendsLength];
+        
+        int[][] giftResult = new int[friendsLength][friendsLength];
+
+        for (int i = 0; i < friendsLength; i++) {
+            dic.put(friends[i], i);
+        }
+
+        for (String gift : gifts) {
+            String[] giftName = gift.split(" ");
+            giftCheck[dic.get(giftName[0])]++;
+            giftCheck[dic.get(giftName[1])]--;
+            giftResult[dic.get(giftName[0])][dic.get(giftName[1])]++;
+        }
+
+        for (int i = 0; i < friendsLength; i++) {
+            int num = 0;
+            for (int j = 0; j < friendsLength; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (giftResult[i][j] > giftResult[j][i] ||
+                        (giftResult[i][j] == giftResult[j][i] && giftCheck[i] > giftCheck[j])) {
+                    num++;
+                }
+            }
+
+            if (answer < num) {
+                answer = num;
+            }
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+    	
+        String[] friends = {"muzi", "ryan", "frodo", "neo"};
+        String[] gifts = {"muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"};
+        
+        int result = solution(friends, gifts);
+        
+        System.out.println("다음 달에 가장 많이 받을 선물의 수: " + result);
+    }
+}
+
+  
+
+
